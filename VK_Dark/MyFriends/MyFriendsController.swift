ff0aad8bbc0b03c8acebcbdc5bc69ac69e0e1805
StanislavBelykh <apple.stas@outlook.com>
2019-04-16 13:52:52 +0300

    Lesson_5 Добавил секции

--- VK_Dark/MyFriends/MyFriendsController.swift
+++ VK_Dark/MyFriends/MyFriendsController.swift
@@ -15,31 +15,73 @@ class MyFriendsController: UITableViewController {
     Friend(name: "Friend 2", Photos: [UIImage(named: "Friend"),UIImage(named: "Friend")]),
     Friend(name: "Friend 3", Photos: [UIImage(named: "Friend"),UIImage(named: "Friend"),UIImage(named: "Friend")]),
     Friend(name: "Friend 4", Photos: [UIImage(named: "Friend"),UIImage(named: "Friend"),UIImage(named: "Friend"),UIImage(named: "Friend")]),
-    Friend(name: "Friend 5", Photos: [UIImage(named: "Friend"),UIImage(named: "Friend"),UIImage(named: "Friend"),UIImage(named: "Friend"),UIImage(named: "My_Friends")])
+    Friend(name: "Griend 5", Photos: [UIImage(named: "Friend"),UIImage(named: "Friend"),UIImage(named: "Friend"),UIImage(named: "Friend"),UIImage(named: "My_Friends")]),
+    Friend(name: "Ariend 1", Photos: [UIImage(named: "Friend")]),
+    Friend(name: "Briend 1", Photos: [UIImage(named: "Friend")]),
+    Friend(name: "Criend 1", Photos: [UIImage(named: "Friend")]),
+    Friend(name: "Driend 1", Photos: [UIImage(named: "Friend")]),
+    Friend(name: "Eriend 1", Photos: [UIImage(named: "Friend")]),
+    Friend(name: "Hriend 1", Photos: [UIImage(named: "Friend")]),
+    Friend(name: "Lriend 1", Photos: [UIImage(named: "Friend")]),
+    Friend(name: "Mriend 1", Photos: [UIImage(named: "Friend")]),
+    Friend(name: "Nriend 1", Photos: [UIImage(named: "Friend")])
   ]
+  public var friendsDictionary = [String: [String]]()
+  public var friendSectionTitles = [String]()
+  public var friends = [String]()
+  
+  var firstLetterOfName:[Character] = []
+  
   
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+      for friend in myFriends {
+        let friendKey = String(friend.name.prefix(1))
+        if var friendValues = friendsDictionary[friendKey] {
+          friendValues.append(friend.name)
+          friendsDictionary[friendKey] = friendValues
+        }else{
+          friendsDictionary[friendKey] = [friend.name]
+        }
+        
+      }
+      
+      friendSectionTitles = [String](friendsDictionary.keys)
+      friendSectionTitles = friendSectionTitles.sorted(by:{ $0 < $1})
+  
 
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
       
-        return 1
+        return friendSectionTitles.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      let friendKey = friendSectionTitles[section]
+      if let friendValues = friendsDictionary[friendKey] {
+        return friendValues.count
+      }
     
-        return myFriends.count
+        return 0
     }
   
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myFriendsCells", for: indexPath) as! MyFriendsCell
-        cell.myFriendNameLabel.text = myFriends[indexPath.row].name
-
-
+      let friendKey = friendSectionTitles[indexPath.section]
+      if let friendValues = friendsDictionary[friendKey] {
+        cell.myFriendNameLabel.text = friendValues[indexPath.row]
+      }
+      
         return cell
     }
+  override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return friendSectionTitles[section]
+  }
+  override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+    return friendSectionTitles
+  }
   
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
       if segue.identifier == "ShowPhoto",
